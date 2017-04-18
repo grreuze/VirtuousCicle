f4b7286e8bc760a7cf679be36a8c4ae9f9248423
grreuze <grreuze@gmail.com>
2017-04-18 14:16:10 +0200

    Camera

---
+++ Assets/Scripts/SC_MultiTargetCamera.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class SC_MultiTargetCamera : MonoBehaviour {
+
+    public Transform[] targets;
+    public Vector3 offset;
+    public MinMax distance = new MinMax(7, 20);
+    public float currentDistance = 14,
+                 smooth = 2,
+                 distanceModifier = 1.5f;
+
+    Transform my;
+
+    void Start() {
+        my = transform;
+    }
+
+    void Update() {
+        
+        Vector3 camPos = (targets[1].position + targets[0].position) / 2 + offset;
+        my.LookAt(camPos);
+
+        currentDistance = distance.Clamp(Vector3.Distance(targets[0].position, targets[1].position) * distanceModifier);
+
+        camPos.z -= currentDistance;
+
+        my.position = Vector3.Slerp(my.position, camPos, smooth * Time.deltaTime);
+    }   
+    
+}
---
+++ Assets/Structs/MinMax/MinMax.cs
@@ -0,0 +1,49 @@
+﻿[System.Serializable]
+public struct MinMax {
+    public float min, max;
+
+    /// <summary>
+    /// A Vector2 containing a "minimum" value and a "maximum" value.
+    /// </summary>
+    /// <param name="min"> The minimum value.</param>
+    /// <param name="max"> The maximum value.</param>
+    public MinMax(float min, float max) {
+        this.min = min;
+        this.max = max;
+    }
+
+    /// <summary>
+    /// Returns a MinMax with switched min and max values.
+    /// </summary>
+    /// <returns> The inverted MinMax. </returns>
+    public MinMax MaxMin {
+        get { return new MinMax(max, min); }
+    }
+
+    /// <summary>
+    /// Switches the min and max values.
+    /// </summary>
+    public void Inverse() {
+        float tmp = min;
+        min = max;
+        max = tmp;
+    }
+
+    /// <summary>
+    /// Clamps a value between Min and Max.
+    /// </summary>
+    /// <param name="value"> The value to clamp with the MinMax. </param>
+    /// <returns> The clamped value. </returns>
+    public float Clamp(float value) {
+        return value <= min ? min : value >= max ? max : value;
+    }
+
+    /// <summary>
+    /// Linearly interpolates between Min and Max by t.
+    /// </summary>
+    /// <param name="t"> The interpolation value between Min and Max. </param>
+    /// <returns> The interpolated value. </returns>
+    public float Lerp(float t) {
+        return (1 - t)*min + t*max;
+    }
+}
