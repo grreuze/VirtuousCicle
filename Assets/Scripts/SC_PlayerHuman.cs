a021439eed1ba74b2c9f56e6b81be4fe83482e3b
Unknown <romain.tsu@hotmail.fr>
2017-04-18 19:21:54 +0200

    Human underwater basic features
    Changement de vitesse en fonction de "l'oxygène".
    Jauge d'oxygène.

--- Assets/Scripts/SC_PlayerHuman.cs
+++ Assets/Scripts/SC_PlayerHuman.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class SC_PlayerHuman : MonoBehaviour {
@@ -9,17 +10,25 @@ public class SC_PlayerHuman : MonoBehaviour {
     public float maxSpeed; //vitesse maximale de déplacement
     public float maxSpeedUnderwater; //vitesse maximale de déplacement sous l'eau
     public float minSpeed; //vitesse minimale de déplacement
-    public float speedLossInWater; //vitesse à laquelle le personnage ralentit 
+    float airStock = 100; //pourcentage d'air restant au personnage
+    public float airPercentLoss; //pourcentage d'air perdu à chaque frame
+    public AnimationCurve speedLossFromAir; //tableau de la perte de vitesse en fonction de l'air restant
     public float jumpHeight; //hauteur du saut
+    public float distFloorForJump; //distance minimale avec le sol pour pouvoir sauter
 
     //Variables d'état
-    public bool isUnderwater; //vrai si le personnage est sous la surface, faux s'il est au-dessus
-    public bool isGrounded; //vrai si le personnage est en contact avec le sol
+    bool isUnderwater; //vrai si le personnage est sous la surface, faux s'il est au-dessus
 
     //Autre variables
     Rigidbody rb; //rigidbody de l'acteur
     Collider coll; //collider de l'acteur
 
+    GameObject airGauge; 
+    Slider airSlider; //jauge d'oxygène
+    Transform sliderFill; //référence au "fill" du slider
+    public Color airSliderColorSafe; //couleur de la jauge d'oxygène avant la perte de vitesse
+    public Color airSliderColorDanger; //couleur de la jauge d'oxygène à la fin de la perte de vitesse
+    
 
 
     void Start()
@@ -37,39 +46,46 @@ public class SC_PlayerHuman : MonoBehaviour {
         }
     }
 
-
+    
 
     void Update()
     {
-        if (Input.GetButtonDown("Jump2") && isGrounded && !isUnderwater)
+        //Vérification des conditions pour sauter
+        int layerMask = 1 << 8; //on identifie le layer 8 "player" comme étant celui à ignorer
+        layerMask = ~layerMask;
+
+        if (Input.GetButtonDown("Jump2") && Physics.Raycast(transform.position, -Vector3.up, distFloorForJump, layerMask) && !isUnderwater) 
         {
             Jump();
         }
+
+        /*
+        //En cas de besoin de tweak du raycast de détection du sol
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, -Vector3.up, out hit, distFloorForJump, layerMask))
+        {
+            Debug.Log("bon, là ça touche" + hit.collider.gameObject.name);
+        }
+        else
+        {
+            Debug.Log("touche pas");
+        }*/
     }
 
 
 
     void FixedUpdate()
     {
-        //Gestion de la vitesse de déplacement
-        if (!isUnderwater)
+        //Gestion de l'air sous l'eau
+        if (isUnderwater && airStock > 0)
         {
-            speed = maxSpeed;
-        }
-        else
-        {
-            if (speed > minSpeed)
-            {
-                speed = speed - (speedLossInWater * Time.deltaTime);
-                //Debug.Log("speed" + speed);
-                //Debug.Log("speedLoss" + speedLossInWater * Time.deltaTime);
-            }
-            else
-            {
-                speed = minSpeed;
-            }
+            airStock = airStock - airPercentLoss;
+            SpeedChange(false);
+            airSlider.value = airStock / 100;
+            sliderFill.GetComponent<Image>().color = Color.Lerp(airSliderColorDanger, airSliderColorSafe, speedLossFromAir.Evaluate(airStock / 100)); //adapter la couleur de la jauge à l'air 
         }
 
+        //Déplacements
         //Debug.Log(Input.GetAxisRaw("Horizontal"));
         Vector3 movement = new Vector3(Input.GetAxis("Horizontal2"), 0, 0);
         gameObject.transform.position += movement * speed * Time.deltaTime;
@@ -78,70 +94,94 @@ public class SC_PlayerHuman : MonoBehaviour {
 
 
 //Fonctions
-    void Jump()
+    void Jump() //saut
     {
-        //Vector3 jumpDirection = new Vector3(0, 100 * jumpHeight, 0);
-		//rb.AddForce(jumpDirection);
         rb.AddForce(0, 100 * jumpHeight, 0);
-        isGrounded = false;
     }
 
 
 
-//Détection des collisions & stuff
-    private void OnTriggerEnter(Collider c) //trigger "aux pieds"
+    void SpeedChange(bool gain) //modification de la vitesse de déplacement
     {
-        //Debug.Log(c.gameObject.tag);
-        if (c.gameObject.tag == "Ground") //en contact avec le sol
+        if (!gain) //est-ce une perte de vitesse ?
         {
-            isGrounded = true;
+            speed = Mathf.Lerp(minSpeed, maxSpeedUnderwater, speedLossFromAir.Evaluate(airStock / 100)); //la vitesse s'adapte en fonction de l'air restant
         }
-		else if (c.gameObject.tag == "Water") //entre dans l'eau
+        else //si c'est un regain de vitesse ou un reset, la valeur dépend du milieu
+        {
+            if (isUnderwater)
+            {
+                speed = maxSpeedUnderwater;
+            }
+            else
+            {
+                speed = maxSpeed;
+            }
+        }
+    }
+
+
+
+    private void OnTriggerEnter(Collider c)
+    {
+		if (c.gameObject.tag == "Water") //entre dans l'eau
         {
 			isUnderwater = true;
-            speed = maxSpeedUnderwater;
-			//isGrounded = false;
+            SpeedChange(true);
+            airGauge.SetActive(true);
         }
     }
 
     private void OnTriggerExit(Collider c)
     {
-        //Debug.Log("Im outta " + c.gameObject.tag);
-        if (c.gameObject.tag == "Ground") //n'est plus en contact avec le sol
+        if (c.gameObject.tag == "Water") //sort de l'eau
         {
-            isGrounded = false;
+            isUnderwater = false;
+            SpeedChange(true);
+            airStock = 100;
+            airSlider.gameObject.SetActive(false);
         }
-		else if (c.gameObject.tag == "Water") //sortie de l'eau
-        {
-			isUnderwater = false;
-			speed = maxSpeed;
-		}
     }
 
-
-
-    //Détection des objets collisionnés
+    //Détection des collisions & stuff
     private void OnCollisionEnter(Collision collision)
     {
         GameObject collidedObj = collision.gameObject;
         Debug.Log(collidedObj);
-        if (collision.gameObject.name.Contains("Item"))
+        if (collision.gameObject.name.Contains("Item")) //en contact avec un object
         {
-            if (collision.gameObject.tag == "IAir")
+            if (collision.gameObject.tag == "IAir") //récupère une bulle d'air
             {
-                speed = maxSpeedUnderwater;
+                SpeedChange(true);
                 Destroy(collision.gameObject);
+                airStock = 100;
             }
         }
-        else if (collidedObj.name.Contains("Otter")) //WIP
+
+       /* if (collision.gameObject.tag == "Water") //entre dans l'eau
+        {
+            isUnderwater = true;
+            speed = maxSpeedUnderwater;
+        }*/
+
+                /*else if (collidedObj.name.Contains("Otter")) //WIP
         {
             Debug.Log(collidedObj.GetComponentInChildren<GameObject>().tag);
 
             //GameObject OtterChild = collidedObj
-            /*if (collidedObj.GetComponentInChildren<GameObject>().tag == "IAir")
+            if (collidedObj.GetComponentInChildren<GameObject>().tag == "IAir")
             {
                 Debug.Log("that'll do it");
-            }*/
-        }
+            }
+        }*/
+    }
+
+
+
+    public void SetSlider(GameObject gObj, Transform fill) //Informations envoyées par le slider
+    {
+        airGauge = gObj;
+        airSlider = airGauge.GetComponent<Slider>();
+        sliderFill = fill;
     }
 }
