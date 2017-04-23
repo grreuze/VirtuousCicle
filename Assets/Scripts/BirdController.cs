90fa1d9341bba9a58cb8d24b964109d5d3b0ef1e
Lynorek <schmuck.sylvain@gmail.com>
2017-04-23 19:28:25 +0200

    Première version de l'oiseau

--- Assets/Scripts/BirdController.cs
+++ Assets/Scripts/BirdController.cs
@@ -1,16 +1,68 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class BirdController : MonoBehaviour {
+public class BirdController : PlayerCharacter
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    [Header("Basic Movement")]
+    public float maxVelocity;
+    public float acceleration;
+    [Header("Starting Boost")]
+    public float boostForce;
+    public float boostVelocityThreshold;
+    [Header("Dash")]
+    public float dashForce;
+    public float dashCooldown;
+    [Header("Gravity")]
+    public float gravityScale;
+    public float maxGravityVelocity;
+    public float flapForce;
+
+    Rigidbody rb;
+    Vector3 inputDirection;
+    float inputDelta;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+       // if (!controller.isActive) return;
+
+        if (rb.velocity.magnitude > maxVelocity)
+            rb.velocity = maxVelocity * rb.velocity.normalized;
+
+        inputDirection = new Vector3(Input.GetAxis(controller.horizontalAxis), Input.GetAxis(controller.verticalAxis), 0f);
+
+        if (inputDirection.magnitude > 0f)
+        {
+            ApplyMovement();
+        }
+        else
+        {
+            ApplyGravity();
+        }
+    }
+
+    void ApplyMovement()
+    {
+        if (rb.velocity.magnitude < boostVelocityThreshold)
+        {
+            rb.velocity = rb.velocity / 2f;
+            rb.AddForce(inputDirection * boostForce, ForceMode.VelocityChange);
+        }
+        else
+        {
+            rb.AddForce(inputDirection * acceleration);
+        }
+    }
+
+    void ApplyGravity()
+    {
+        rb.AddForce(Vector3.up * gravityScale, ForceMode.Acceleration);
+
+        if (rb.velocity.y < -maxGravityVelocity)
+            rb.AddForce(Vector3.up * flapForce, ForceMode.VelocityChange);
+    }
 }
