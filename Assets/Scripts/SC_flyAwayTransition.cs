e8e95de01416b2e8bf593feedf013f6ff1d84b5a
Unknown <romain.tsu@hotmail.fr>
2017-05-03 11:56:51 +0200

    post merge fix

--- Assets/Scripts/SC_flyAwayTransition.cs
+++ Assets/Scripts/SC_flyAwayTransition.cs
@@ -5,20 +5,24 @@ using UnityEngine;
 public class SC_flyAwayTransition : MonoBehaviour
 {
     public GameObject characterToAffect;
+    //public string characterToAffectName;
     GameObject charaReference;
     Rigidbody charaRbReference;
     bool inAnimation = false;
 
+    float progression = 0;
+    Vector3 initialRotation;
+
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log(other);
-        if (other == characterToAffect && !inAnimation)
+        if (other.transform.parent.gameObject == characterToAffect && !inAnimation)
         {
             Debug.Log("collision");
             charaReference = other.transform.parent.gameObject;
             charaRbReference = charaReference.GetComponent<Rigidbody>();
             //charaReference.GetComponent<> //virer les colliders pour éviter les entrées multiples
             inAnimation = true;
+            initialRotation = charaReference.transform.rotation.eulerAngles;
             StartCoroutine(TurnBackAndGo());
         }
     }
@@ -26,15 +30,19 @@ public class SC_flyAwayTransition : MonoBehaviour
     IEnumerator TurnBackAndGo()
     {
         Debug.Log("StartTransition");
-        float rotation = 0f;
-        while (rotation >= -180f)
+        //float rotation = 0f;
+        while (progression <= 1)
         {
-            charaReference.transform.Rotate(new Vector3(-180f, 0f, 0f));
-            rotation = rotation - 5f;
-            yield return new WaitForSeconds(0.05f);
+            Debug.Log(charaReference.transform.rotation.eulerAngles);
+            charaReference.transform.Rotate(Vector3.Lerp(initialRotation, new Vector3 (initialRotation.x, 90f, -90f), progression));
+               // new Vector3(0f, rotation, 0f));
+            //rotation = rotation - 1f;
+            progression += 0.05f; 
+            yield return new WaitForSeconds(0.1f);
         }
         for (int i = 0; i < 5; i++)
         {
+            Debug.Log("two");
             charaRbReference.AddForce(-Vector3.right * 2f);
             yield return new WaitForSeconds(0.75f);
         }
